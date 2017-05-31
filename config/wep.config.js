4feab5d003fa11095aec8ca6d3283b9881b96439
ancheltong(童贤友) <ancheltong@tencent.com>
2017-05-31 22:42:13 +0800

    'update'

--- config/wep.config.js
+++ config/wep.config.js
@@ -1,74 +1,79 @@
 
-let wep = require('wep');
+module.exports = function (wep) {
 
-wep.init({
+  wep.init({
 
-  env: {
-    liugong_pre: {
-      cdn: {
-        userDir: '/wenzhen/pre',
-        doctorDir: '/wenzhen_doctor/pre'
+    env: {
+      liugong_pre: {
+        cdn: {
+          userDir: '/wenzhen/pre',
+          doctorDir: '/wenzhen_doctor/pre'
+        }
+      },
+      liugong: {
+        cdn: {
+          userDir: '/wenzhen'
+        }
       }
     },
-    liugong: {
-      cdn: {
-        userDir: '/wenzhen'
-      }
-    }
-  },
 
-  zip: {
-    options: {
+    zip: {
+      options: {
 
-    },
-    files: {
-      dist: {
-        src: [{
-          file: './config/wep.config.js',
-          name: 'xxoo/config.js',
-        }, {
-          pattern: '**/*',
-          cwd: './node_modules/async/'
-        }],
-        dest: './tmp'
       },
-
-      dist2: {
-        src: [{
-          pattern: '**/*',
-          cwd: './node_modules/request/'
-        }],
-        dest: './tmp'
-      }
-    }
-  },
-
-  upload: {
-    options: {
-      // url: 'http://inner.up.cdn.qq.com:8080/uploadServer/singleUpload.jsp',
-      qs: {
-        appName: 'hospital_img',
-      },
-      headers: {
-        'x-xxx': 'sfds'
-      },
-    },
-    files: [
-      {
-        options: {
-          url: 'http://wechat.anchel.cn/upload/uploadimg',
-          qs: { isunzip: 1 },
-          headers: {},
+      files: {
+        dist: {
+          src: [{
+            file: './package.json',
+            name: 'xxoo/config.json',
+          }, {
+            pattern: '**/*',
+            cwd: './node_modules/async/'
+          }],
+          dest: './tmp'
         },
-        src: ['./tmp/dist.zip', './tmp/dist2.zip'],
-      },
-      {
-        options: {
-          url: 'http://wechat.anchel.cn/upload/uploadimg',
-        },
-        src: ['./dist/app.js'],
-      }
-    ]
-  }
 
-});
+        dist2: {
+          src: [{
+            pattern: '**/*',
+            cwd: './node_modules/request/'
+          }],
+          dest: './tmp'
+        }
+      }
+    },
+
+    upload: {
+      options: {
+        url: 'http://inner.up.cdn.qq.com:8080/uploadserver/uploadfile.jsp',
+        qs: {
+          appname: 'hospital_img',
+          user: 'ancheltong',
+        },
+        headers: {
+          'X-CDN-Authentication': 'EB2522599A6CBAF6DC4450BC7FA1AAF7'
+        },
+        // proxy: 'http://127.0.0.1:8888'
+      },
+      files: [
+        {
+          src: ['./README.md'],
+          dest: '/wenzhen/ancheldev/doctor/xxoo/aa'
+        },
+        {
+          options: {
+            qs: { isunzip: 1 },
+            headers: {},
+          },
+          src: ['./tmp/dist.zip', './tmp/dist2.zip'],
+          dest: '/wenzhen/ancheldev/doctor/xxoo'
+        },
+      ]
+    }
+
+  });
+
+  wep.loadDefaultTask(['env', 'zip', 'upload']);
+
+  wep.registerTask('default', ['env', 'zip', 'upload'])
+};
